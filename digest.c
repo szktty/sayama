cb91225a2a5e1443f2fd0f7e7d537d5307c52003
SUZUKI Tetsuya <suzuki@spice-of-life.net>
2011-08-08 02:34:22 +0900

    impl digest functions

---
+++ digest.c
@@ -0,0 +1,71 @@
+#include "sayama/digest.h"
+#include "sayama/utils.h"
+
+static void digest_sha1_init(sy_digest_context *context);
+static void digest_sha1_update(sy_digest_context *context,
+    const uint8_t *bytes, size_t len);
+static void digest_sha1_final(sy_digest_context *context, uint8_t *dest);
+
+const sy_digester sy_digester_sha1 = {
+  digest_sha1_init,
+  digest_sha1_update,
+  digest_sha1_final,
+  64
+};
+
+static void
+digest_sha1_init(sy_digest_context *context)
+{
+  sy_sha1_init(&context->context.sha1);
+}
+
+static void
+digest_sha1_update(sy_digest_context *context, const uint8_t *bytes,
+    size_t len)
+{
+  sy_sha1_update(&context->context.sha1, bytes, len);
+}
+
+static void
+digest_sha1_final(sy_digest_context *context, uint8_t *dest)
+{
+  sy_sha1_final(&context->context.sha1, dest);
+}
+
+void
+sy_digest_digest(uint8_t *dest, const uint8_t *bytes,
+    size_t len, const sy_digester *digester)
+{
+  sy_digest_context context;
+
+  sy_digest_init(&context, digester);
+  sy_digest_update(&context, bytes, len);
+  sy_digest_final(&context, dest);
+}
+
+void
+sy_digest_init(sy_digest_context *context, const sy_digester *digester)
+{
+  context->digester = digester;
+  digester->init(context);
+}
+
+void
+sy_digest_update(sy_digest_context *context, const uint8_t *bytes,
+    size_t len)
+{
+  context->digester->update(context, bytes, len);
+}
+
+void
+sy_digest_final(sy_digest_context *context, uint8_t *dest)
+{
+  context->digester->final(context, dest);
+}
+
+size_t
+sy_digest_block_len(const sy_digest_context *context)
+{
+  return context->digester->block_len;
+}
+
---
+++ sayama/digest.h
@@ -0,0 +1,46 @@
+#ifndef __SY_DIGEST_H__
+#define __SY_DIGEST_H__
+
+#include <sayama/sha.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct sy_digest_context    sy_digest_context;
+typedef struct sy_digester          sy_digester;
+
+struct sy_digest_context {
+  const sy_digester *digester;
+  union {
+    sy_sha1_context sha1;
+    void *p; /* custom digester */
+  } context;
+};
+
+struct sy_digester {
+  void (*init)(sy_digest_context *context);
+  void (*update)(sy_digest_context *context, const uint8_t *bytes,
+      size_t len);
+  void (*final)(sy_digest_context *context, uint8_t *dest);
+  size_t block_len;
+};
+
+extern const sy_digester sy_digester_sha1;
+
+extern void sy_digest_digest(uint8_t *dest, const uint8_t *bytes,
+    size_t len, const sy_digester *digester);
+extern void sy_digest_init(sy_digest_context *context,
+    const sy_digester *digester);
+extern void sy_digest_update(sy_digest_context *context,
+    const uint8_t *bytes, size_t len);
+extern void sy_digest_final(sy_digest_context *context, uint8_t *dest);
+extern size_t sy_digest_block_len(const sy_digest_context *context);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SY_DIGEST_H__ */
+
