41709ed327b0f7e6a3c8ea7451b76516b511e471
SUZUKI Tetsuya <suzuki@spice-of-life.net>
2011-08-11 01:22:18 +0900

    define SHA256 interface

--- sayama/sha.h
+++ sayama/sha.h
@@ -10,7 +10,8 @@ extern "C"
 {
 #endif
 
-typedef struct sy_sha1_context  sy_sha1_context;
+typedef struct sy_sha1_context      sy_sha1_context;
+typedef struct sy_sha256_context    sy_sha256_context;
 
 #define SY_SHA1_STATE_LEN       20
 #define SY_SHA1_BLOCK_LEN       64
@@ -27,6 +28,12 @@ extern void sy_sha1_init(sy_sha1_context *context);
 extern void sy_sha1_update(sy_sha1_context *context, const uint8_t *bytes,
     size_t len);
 extern void sy_sha1_final(sy_sha1_context *context, uint8_t *dest);
+
+extern void sy_sha256(uint8_t *buf, const uint8_t *data, size_t len);
+extern void sy_sha256_init(sy_sha256_context *context);
+extern void sy_sha256_update(sy_sha256_context *context,
+    const uint8_t *data, size_t len);
+extern void sy_sha256_final(sy_sha256_context *context, uint8_t *buf);
 
 #ifdef __cplusplus
 }
--- sha.h
+++ sha.h
@@ -14,6 +14,15 @@ extern void _sy_sha1_hash_block(uint8_t *block, const uint8_t *data);
 extern void _sy_sha1_f(uint8_t *dest, unsigned int t,
     const uint8_t *b, const uint8_t *c, const uint8_t *d);
 
+extern void _sy_sha256_sigma0(uint8_t *buf, const uint8_t *w);
+extern void _sy_sha256_sigma1(uint8_t *buf, const uint8_t *w);
+extern void _sy_sha256_ch(uint8_t *buf, const uint8_t *w1,
+    const uint8_t *w2, const uint8_t *w3);
+extern void _sy_sha256_maj(uint8_t *buf, const uint8_t *w1,
+    const uint8_t *w2, const uint8_t *w3);
+extern void _sy_sha256_sum0(uint8_t *buf, const uint8_t *w);
+extern void _sy_sha256_sum1(uint8_t *buf, const uint8_t *w);
+
 #ifdef __cplusplus
 }
 #endif
